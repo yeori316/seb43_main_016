f4b670d4dca2f219a2e3814212bc6c2f833276a5
dozy <cs.d.yeol316@gmail.com>
2023-07-02 22:10:15 +0900

    patch filepath

--- client/src/pages/utils/loginUtils.tsx
+++
@@ -1,9 +0,0 @@
-export const validateEmptyInput = (text: any): boolean => {
-  if (!text.hasOwnProperty("length")) {
-    throw new Error("text is not a text?");
-  }
-  if (text.trim().length === 0) {
-    return true;
-  }
-  return false;
-};
