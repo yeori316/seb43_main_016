5290e39c54803fdb6873a82fe9ae08bf356134dd
dozy <cs.d.yeol316@gmail.com>
2023-07-11 01:42:58 +0900

    clear git cache

--- edusync/src/main/java/com/codestates/edusync/model/member/service/MemberManager.java
+++
@@ -1,9 +0,0 @@
-package com.codestates.edusync.model.member.service;
-
-import com.codestates.edusync.model.member.entity.Member;
-
-public interface MemberManager {
-    Member get(String email);
-
-    Member getNickName(String email);
-}
